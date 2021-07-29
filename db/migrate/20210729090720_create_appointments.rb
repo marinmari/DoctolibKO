a1b38e9fc7ac94b1e665d7a65ee08c32733bb039
Marin <marin.chretien@gmail.com>
2021-07-29 15:13:39 +0200

    First one

---
+++ db/migrate/20210729090720_create_appointments.rb
@@ -0,0 +1,9 @@
+class CreateAppointments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :appointments do |t|
+      t.belongs_to :doctor, index: true
+      t.belongs_to :patient, index: true
+      t.timestamps
+    end
+  end
+end
